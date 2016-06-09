6f5075b7894240702714f8777d18bf0b91e2e280
Frank Zhao <frank.zhao.main@gmail.com>
2016-06-09 12:01:10 -0700

    added comments to adc.c

--- firmware/adc.c
+++ firmware/adc.c
@@ -1,8 +1,8 @@
 #include "adc.h"
 #include <avr/io.h>
 
-#define ADC_MUX_CONFIG _BV(ADLAR) | _BV(REFS2) | _BV(REFS1)
-#define ADC_SRA_CONFIG _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0)
+#define ADC_MUX_CONFIG _BV(ADLAR) | _BV(REFS2) | _BV(REFS1) // left adjusted, uses 2.56V internal reference
+#define ADC_SRA_CONFIG _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0) // very slow conversion
 
 void adc_init(void)
 {
@@ -12,8 +12,8 @@ void adc_init(void)
 
 uint16_t adc_read(uint8_t chan)
 {
-    ADMUX = ADC_MUX_CONFIG | chan;
+    ADMUX = ADC_MUX_CONFIG | chan; // select channel
     ADCSRA |= (1 << ADSC);         // start ADC measurement
-    while (ADCSRA & (1 << ADSC) ); // wait till conversion complete 
-    return ADC;
+    while (ADCSRA & (1 << ADSC)); // wait till conversion complete 
+    return ADC; // GCC is smart enough to return this in the correct order
 }
